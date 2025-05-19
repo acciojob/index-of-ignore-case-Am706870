9c8c6cfca1c2a9e5a29a04e349259b3c9498f194
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-05-19 17:17:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
-
-// Please do not change the code below
+	let s1lower = s1.toLowerCase();
+	let s2lower = s2.toLowerCase();
+	return s1lower.indexOf(s2lower)
+			}
+	
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
